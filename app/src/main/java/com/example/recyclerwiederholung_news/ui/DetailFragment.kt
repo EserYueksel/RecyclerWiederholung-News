86291c9c084c4e077ec73650d29d92e94f6ff405
EserYueksel <eseryuksel@live.de>
2023-02-01 09:35:44 +0100

    - Fragments and Classes reworked - Error Fixed

--- app/src/main/java/com/example/recyclerwiederholung_news/ui/DetailFragment.kt
+++ app/src/main/java/com/example/recyclerwiederholung_news/ui/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.recyclerwiederholung_news.ui
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
